97a37ebd1969dfb799340a31dec7739f4e315f06
CrazyBloxyCola <crazybloxycola1982@gmail.com>
2026-06-01 19:46:26 +0800

    adjusted css

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 
 export const Login = () => {
     const [form, setForm] = useState({username: "", password: ""});
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -14,14 +15,17 @@ export const Login = () => {
     return (
         <>
             <nav className="text-center items-center justify-between flex flex-row w-full bg-gray-800 text-white">
-                <div className="flex flex-row m-6">
+                <div className="flex flex-row m-7">
                     <NavLink to="/" className="text-xs flex flex-row items-center font-bold">
                         Back
                     </NavLink>
                 </div>
             </nav>
             
-            <div className="min-h-screen flex items-center justify-center">
+            <div className="justify-center flex-1 text-center bg-gray-500 text-4xl mt-10 ml-145 w-50 h-15">
+                <h1> Staff Login </h1>
+            </div>
+            <div className="flex items-center justify-center mt-15">
                 <form onSubmit={handleSubmit} className="space-y-10 border-2 bg-white">
 
                     {/* Username */}
@@ -30,7 +34,7 @@ export const Login = () => {
                         placeholder="Username"
                         value={form.username}
                         onChange={(e) => setForm({...form, username: e.target.value})}
-                        className="relative flex item-center text-white placeholder-gray-400"
+                        className="relative flex item-center placeholder-gray-400 mt-3"
                         />
                     </div>
 
@@ -40,7 +44,7 @@ export const Login = () => {
                         placeholder="Password"
                         value={form.password}
                         onChange={(e) => setForm({...form, password: e.target.value})}
-                        className="relative flex item-center text-white placeholder-gray-400"
+                        className="relative flex item-center placeholder-gray-400"
                         />
                     </div>
 
